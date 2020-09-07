114da5c7dd23e61b70501745a23745f021384d2b
mahes <mahes@DESKTOP-7E6UMB7>
2020-09-07 15:57:33 +0530

    Modify Printnums java program

--- JavaPrograms/src/mahesh/Printnums.java
+++ JavaPrograms/src/mahesh/Printnums.java
@@ -29,8 +29,8 @@ public class Printnums {
 	public static void main(String[] args) {
 		java.util.Scanner sc = new java.util.Scanner(System.in);
 		System.out.println("Enter the numeber:");
-		int n = sc.nextInt();
-		for(int i=0;i<n;i++)
+		int x = sc.nextInt();
+		for(int i=0;i<x;i++)
 		{
 			System.out.print(i);
 		}
